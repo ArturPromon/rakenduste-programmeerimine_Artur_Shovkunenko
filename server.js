963ef563211e3a17a4cb9c1fe68ff251040c9acd
ArturPromon <43129600+ArturPromon@users.noreply.github.com>
2019-09-12 22:06:21 +0300

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require ("path");
 const app = express()
-const port = 3000
+const PORT = 3000
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "static", "index.html"));
